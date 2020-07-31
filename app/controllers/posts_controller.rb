857745e619b4d4db84d530343d8533024c66c842
k2pajero <68995269+k2pajero@users.noreply.github.com>
2020-07-31 12:04:17 +0900

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,16 @@
+class PostsController < ApplicationController
+  def index  # indexアクションを定義した
+    @posts = Post.all
+    #全てのレコードを取得して＠postsに代入
+    # @post = Post.find(1)
+    #1番目のレコードを＠postに代入
+    #"これはコントローラーで定義したインスタンス変数を確認するための文字列です"
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(content: params[:content])
+  end
+end
